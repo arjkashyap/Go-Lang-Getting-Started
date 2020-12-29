2cba5befef8c2f31f9b9bc7dc4bce13d73378321
Arjun <arj.kashyap123@gmail.com>
2020-12-29 14:42:41 +0530

    Hello go

---
+++ 002-Variables.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// The way you declare varaiable in go is
+	// variable name followed by "var" keyword followed by type
+
+	var a int = 2
+	fmt.Println(a)
+}
